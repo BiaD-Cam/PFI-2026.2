2b5f1595a4b65008e11bc33038bbd31b39bfe602
BiaD-Cam <bia.camara72@gmail.com>
2026-08-17 22:42:56 +0000

    17082026

--- aula01c.c/primeiroprog.c
+++ aula01c.c/primeiroprog.c
@@ -1,6 +1,9 @@
-# include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int main()
+{
+printf("Primeiro programa.");
+system("PAUSE");
+return 0;
 }
---
+++ aula01c.c/sintaxe_geral.c
@@ -0,0 +1,16 @@
+//tipo nomeFunc(parâmetros)
+//{
+// declaração de variáveis
+//tipo_var variável;
+// instruções
+//instrução_1 ;
+//instrução_n ;
+//return valor;
+//}
+
+int main()
+{
+// código principal
+printf("Olá, Mundo!\n");
+return 0;
+}
